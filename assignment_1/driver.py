6dc84c4a98e1f2c145f5872c5c53f9523ebc122c
Sharan Kumar D <sharankumar.d@diggibyte.com>
2024-02-16 19:53:18 +0530

    Deleted assignment_1 directory

--- assignment_1/driver.py
+++
@@ -1,5 +0,0 @@
-def dj(customer, product_model):
-    return purchase_data_df.groupBy(customer).agg(collect_set(product_model)).filter(expr("collect_set(product_model) = array('A')"))
-
-
-dj('customer')
