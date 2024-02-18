b03469bcd0d75b4da071202f3a19408c2ba6b22e
Sharan Kumar D <sharankumar.d@diggibyte.com>
2024-02-18 13:51:22 +0530

    Updated driver.py

--- src/assignment_4/driver.py
+++ src/assignment_4/driver.py
@@ -20,7 +20,7 @@ record_count_not_flattened = df_with_schema.count()
 # explode, explode_outer, and posexplode
 df_explode = df_with_schema.select("id", explode("employees").alias("exploded_employee"))
 df_explode_outer = df_with_schema.select("id", explode_outer("employees").alias("exploded_outer_employee"))
-df_posexplode = df_with_schema.select("id", posexplode("employees").alias("pos_employee"))
+df_posexplode = df_with_schema.select("id", posexplode("employees").alias("pos", "employee"))
 
 # Convert column names from camel case to snake case
 df_snake_case = convert_to_snake_case(df_flattened)
@@ -34,7 +34,15 @@ df_with_date_parts = date_parts_columns(df_with_load_date)
 # Write DataFrame to a table
 df_with_date_parts.write.mode("overwrite").format("json").partitionBy("year", "month", "day").saveAsTable("employee.employee_details")
 
+# Read the json file
 json_df = read_json_without_schema(spark, r"C:\Users\SharanKumar\Desktop\JSON files\Nested_json_file.json")
 
-exploded_json_df = explode_data(json_df)
-filtered_json_df = json_df(exploded_json_df, "1001")
+# Explode nested data
+nested_column = "employees"
+exploded_json_df = explode_data(json_df, nested_column)
+
+# Filter the DataFrame based on a condition
+filter_column_name = "exploded_column.id"
+condition_value = "0001"
+filtered_json_df = json_df.filter(exploded_json_df, filter_column_name, condition_value)
+filtered_json_df.show()
