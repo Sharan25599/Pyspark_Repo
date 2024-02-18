7e58685e666216c57e3437e760f861e2d02b1d72
Sharan Kumar D <sharankumar.d@diggibyte.com>
2024-02-18 13:52:45 +0530

    Updated util.py

--- src/assignment_4/util.py
+++ src/assignment_4/util.py
@@ -21,23 +21,37 @@ def read_json_with_schema(spark, file_path):
 
 def read_json_without_schema(spark, file_path):
     return spark.read.json(file_path)
+
 # Find record count with and without schema
 def flatten_dataframe(df):
     return df.select(col("id"),col("properties.name").alias("company_name"),col("properties.storeSize").alias("store_size"),explode("employees").alias("employee"))
 
 def explode_data(df):
     return df.select("id", explode("data").alias("exploded_data"))
+
 def explode_data(df):
     return df.select("id", explode("employees").alias("exploded_employee")).\
            df.select("id", explode_outer("employees").alias("exploded_outer_employee")).\
            df.select("id", posexplode("employees").alias("pos_employee"))
+
 def convert_to_snake_case(df):
     return df.toDF(*(col_name.lower() for col_name in df.columns))
+
 def load_date_column(df):
     return df.withColumn("load_date", lit(datetime.now().date()))
+
 def date_parts_columns(df):
     return (df.withColumn("year", year("load_date"))
             .withColumn("month", month("load_date"))
             .withColumn("day", dayofmonth("load_date")))
-    def filter_data_by_id(df, target_id):
-        return df.filter(f"id == '{target_id}'")
+
+def read_json_without_schema(spark, json_path):
+    return spark.read.json(json_path)
+
+def explode_data(df, nested_column):
+    return df.select(explode(col(nested_column)).alias("exploded_column"))
+
+def filter_data(df, filter_column, condition_value):
+    return df.filter(col(filter_column) == condition_value)
+
+
